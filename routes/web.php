aaf16d24fd0471f65228df9ae4fd0365e8a75d95
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-27 03:33:32 +0900

    sessionMiddlewareの作成

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::name('product.')
           Route::get('/', 'ProductController@index')->name('index');
           Route::get('/product/{id}', 'ProductController@show')->name('show');
       });
-
+Route::name('line_item.')
+->group(function () {
+    Route::post('/line_item/create', 'LineItemController@create')->name('create');
+});
